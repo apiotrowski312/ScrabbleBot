31c5c963bf2df030b99b75f1a490abc9946defdd
apiotrowski312 <apiotrowski312@gmail.com>
2020-05-09 12:04:34 +0200

    Improve test_utils package

--- utils/test_utils/test_utils.go
+++ utils/test_utils/test_utils.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -11,21 +12,8 @@ const (
 	permission = 0666
 )
 
-// TODO: Create dir if not exist
-func GetGoldenFile(t *testing.T, actual []byte, fileName string, shouldUpdate bool) []byte {
-
-	golden := filepath.Join("testdata", fileName+".golden")
-	if shouldUpdate {
-		if err := ioutil.WriteFile(golden, actual, permission); err != nil {
-			t.Fatalf("Error writing golden file for filename=%s: %s", fileName, err)
-		}
-	}
-
-	return readFile(t, golden)
-}
-
 func GetGoldenFileJSON(t *testing.T, actual interface{}, expected interface{}, fileName string, shouldUpdate bool) {
-	golden := filepath.Join("testdata", fileName+".golden")
+	golden := filepath.Join("testdata", fileName)
 
 	if shouldUpdate {
 		bytes, err := json.MarshalIndent(actual, "", "\t")
@@ -33,10 +21,7 @@ func GetGoldenFileJSON(t *testing.T, actual interface{}, expected interface{}, f
 		if err != nil {
 			t.Fatalf("Error while marshal a struct %v: %s", actual, err)
 		}
-
-		if err = ioutil.WriteFile(golden, bytes, permission); err != nil {
-			t.Fatalf("Error writing golden file for filename=%s: %s", fileName, err)
-		}
+		writeFile(t, golden, bytes)
 	}
 
 	err := json.Unmarshal(readFile(t, golden), &expected)
@@ -48,29 +33,33 @@ func GetGoldenFileJSON(t *testing.T, actual interface{}, expected interface{}, f
 }
 
 func GetGoldenFileString(t *testing.T, actual string, fileName string, shouldUpdate bool) string {
-	golden := filepath.Join("testdata", fileName+".golden")
+	golden := filepath.Join("testdata", fileName)
 	if shouldUpdate {
-		if err := ioutil.WriteFile(golden, []byte(actual), permission); err != nil {
-			t.Fatalf("Error writing golden file for filename=%s: %s", fileName, err)
-		}
+		writeFile(t, golden, []byte(actual))
 	}
 
 	return string(readFile(t, golden))
 }
 
 func LoadJSONFixture(t *testing.T, fileName string, structToLoad interface{}) {
-	expected, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		t.Fatal(err)
-	}
-
+	expected := readFile(t, fileName)
 	json.Unmarshal(expected, &structToLoad)
 }
 
 func readFile(t *testing.T, fileName string) []byte {
-	expected, err := ioutil.ReadFile(fileName)
+	expected, err := ioutil.ReadFile(preparePath(fileName))
 	if err != nil {
 		t.Fatal(err)
 	}
 	return expected
+}
+
+func writeFile(t *testing.T, fileName string, bytes []byte) {
+	if err := ioutil.WriteFile(preparePath(fileName), bytes, permission); err != nil {
+		t.Fatalf("Error writing golden file for filename=%s: %s", fileName, err)
+	}
+}
+
+func preparePath(path string) string {
+	return strings.ReplaceAll(path, " ", "_") + ".golden"
 }
