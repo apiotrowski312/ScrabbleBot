bf3df968172c78d03408e582d83b0d444449dffc
apiotrowski312 <apiotrowski312@gmail.com>
2020-06-01 18:23:01 +0200

    Add PlaceWord and remove singleton

--- grabble/board.go
+++ grabble/board.go
@@ -7,16 +7,11 @@ type field struct {
 
 type Board [15][15]*field
 
-var (
-	board           = Board{}
-	transposedBoard = Board{}
-)
+var ()
 
 func CreateBoard(template [15][15]rune) *Board {
-	if (Board{}) != board {
-		return &board
-	}
 
+	var board Board
 	for x, row := range template {
 		for y, f := range row {
 			board[x][y] = &field{Bonus: f}
@@ -26,11 +21,10 @@ func CreateBoard(template [15][15]rune) *Board {
 	return &board
 }
 
-func (b *Board) TransposeBoard() *Board {
-	if (Board{}) != transposedBoard {
-		return &transposedBoard
-	}
+// TODO: Create little helper package to pretty print fixtures and golden files
 
+func (b *Board) TransposeBoard() *Board {
+	var transposedBoard Board
 	for i, row := range b {
 		for j := range row {
 			transposedBoard[i][j] = b[j][i]
@@ -38,3 +32,21 @@ func (b *Board) TransposeBoard() *Board {
 	}
 	return &transposedBoard
 }
+
+func (b *Board) PlaceWord(word string, startPos [2]int, horizontal bool) {
+	if horizontal {
+		b.placeWord(word, startPos)
+	} else {
+		tb := b.TransposeBoard()
+		tb.placeWord(word, [2]int{startPos[1], startPos[0]})
+	}
+}
+
+func (b *Board) placeWord(word string, startPos [2]int) {
+	for i, letter := range word {
+		b[startPos[0]][startPos[1]+i].Letter = letter
+	}
+}
+
+// TODO: Collect all new words
+// TODO: If word can be placed
--- grabble/board_test.go
+++ grabble/board_test.go
@@ -20,9 +20,9 @@ func Test_CreateBoard(t *testing.T) {
 		{
 			"Proper template",
 			[15][15]rune{
-				[15]rune{'W', 'w', 'W', '0', '0', 'W', 'w', 'W', '0', '0', 'W', 'w', 'W', '0', '0'},
-				[15]rune{'l', 'L', '0', 'l', 'L', '0', 'l', 'L', '0', 'l', 'L', '0', 'l', 'L', '0'},
-				[15]rune{'W', 'w', 'W', '0', '0', 'W', 'w', 'W', '0', '0', 'W', 'w', 'W', '0', '0'},
+				{'W', 'w', 'W', '0', '0', 'W', 'w', 'W', '0', '0', 'W', 'w', 'W', '0', '0'},
+				{'l', 'L', '0', 'l', 'L', '0', 'l', 'L', '0', 'l', 'L', '0', 'l', 'L', '0'},
+				{'W', 'w', 'W', '0', '0', 'W', 'w', 'W', '0', '0', 'W', 'w', 'W', '0', '0'},
 			},
 		},
 	}
@@ -42,13 +42,81 @@ func Test_TransposeBoard(t *testing.T) {
 	var board grabble.Board
 	test_utils.LoadJSONFixture(t, "testdata/board.fixture", &board)
 
-	var expectedTransposedBoard grabble.Board
-
 	transposedBoard := board.TransposeBoard()
-	test_utils.GetGoldenFileJSON(t, transposedBoard, &expectedTransposedBoard, "transposed_board", true)
-
-	assert.Equal(t, &expectedTransposedBoard, transposedBoard)
-
 	board[5][7].Letter = 'c'
 	assert.Equal(t, board[5][7], transposedBoard[7][5])
+
+	transposedBoard[5][7].Letter = 'a'
+	assert.Equal(t, transposedBoard[5][7], board[7][5])
+
+	var expectedTransposedBoard grabble.Board
+	test_utils.GetGoldenFileJSON(t, transposedBoard, &expectedTransposedBoard, "transposed_board", *update)
+	assert.Equal(t, &expectedTransposedBoard, transposedBoard)
+
+}
+
+func Test_PlaceWord(t *testing.T) {
+	type testCase struct {
+		name       string
+		words      []string
+		startPos   [][2]int
+		horizontal bool
+	}
+	test := []testCase{
+		{
+			"Place one word",
+			[]string{"words"},
+			[][2]int{
+				{0, 0},
+			},
+			true,
+		},
+		{
+			"Place many word",
+			[]string{
+				"words", "words", "words", "words", "words", "words",
+			},
+			[][2]int{
+				{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0},
+			},
+			true,
+		},
+		{
+			"Simple vertically",
+			[]string{
+				"words",
+			},
+			[][2]int{
+				{0, 0},
+			},
+			false,
+		},
+		{
+			"Words vertically",
+			[]string{
+				"words",
+				"words",
+				"words",
+			},
+			[][2]int{
+				{0, 0}, {0, 1}, {2, 2},
+			},
+			false,
+		},
+	}
+
+	for _, c := range test {
+		t.Run(c.name, func(t *testing.T) {
+			var board grabble.Board
+			test_utils.LoadJSONFixture(t, "testdata/board.fixture", &board)
+			var expectedBoard grabble.Board
+
+			for i, word := range c.words {
+				board.PlaceWord(word, c.startPos[i], c.horizontal)
+			}
+
+			test_utils.GetGoldenFileJSON(t, board, &expectedBoard, c.name, *update)
+			assert.Equal(t, expectedBoard, board)
+		})
+	}
 }
